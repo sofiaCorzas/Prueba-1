dfe0922288ba5616b2cfc862dc070f84868abf92
sofiaCorzas <72525164+sofiaCorzas@users.noreply.github.com>
2020-10-07 16:44:47 -0500

    Update Programaresfácil.c

--- Programaresfácil.c
+++ Programaresfácil.c
@@ -3,5 +3,6 @@ inta main (){
   printf("Este es mi segundo nombre")
   printf("Programar es fácil")
   printf("Voy a compartir con mi compañero el código")
+  printf("\nCorzas Rodriguez Sofia\n")
   return 0;
 }
